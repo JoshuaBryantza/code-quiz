b3c3c14c35d99567714e5acdb983e92c8ad89f7f
Joshua Bryant <joshuabryantza@gmail.com>
2023-12-04 22:32:07 -0800

    added main structure to html and got the start quiz and first page to hide with javascript

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,38 @@
-console.log('Hello World')
-
 var pageOneEl = document.querySelector('#first')
 var pageTwoEl = document.querySelector('#secondPage')
 var pageThreeEl = document.querySelector('#thirdPage')
 var pageFourEl = document.querySelector('#forthPage')
+var startPageEl = document.querySelector('#startPage')
+var goToPageTwoEl = document.querySelector('.goToPageTwo')
 
 document.querySelector("#startQuiz")
-    .addEventListener("click", switchPages)
+    .addEventListener("click", startQuiz)
 
 
-function switchPages() {
+
+
+
+function startQuiz() {
+    startPageEl.classList.toggle("hide");
+    pageOneEl.classList.toggle("hide");
+    var choices = document.querySelectorAll(".goToPageTwo")
+    // console.log(choices)
+    for (var i = 0; i < 4; i++) {
+        console.log(choices[i])
+        choices[i].addEventListener("click", nextPage)
+    }
+    // choices.addEventListener("click", nextPage)
+
+}
+
+
+function nextPage() {
     pageOneEl.classList.toggle("hide");
     pageTwoEl.classList.toggle("hide");
 }
+
+
+
 
 var strings = pageTwoEl.children[1].children[0]
 var booleans = pageTwoEl.children[1].children[1]
@@ -24,8 +44,10 @@ var numbers = pageTwoEl.children[1].children[3]
 
 //
 //When the start button is click 
-//Then a timer starts in the corner and the first question is presented
-//When user answers a question then they are moved to the next question
+//Then a timer starts in the right hand corner
+//And the first question is presented with a list of options to choose from 
+//When user answers a question correctly 
+//Then they are moved to the next question
 //When the user answers the question incorrectly 
 //Then time is subtracted from the clock
 //If the user complets all the questions or the timer reaches 0
